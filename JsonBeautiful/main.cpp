ffa064b65279bcf188e7d4ff0afb4d2d3dea9aa4
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-09-15 13:20:28 +0300

    Initial Commit

---
+++ JsonBeautiful/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  JsonBeautiful
+//
+//  Created by ПК on 15.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
