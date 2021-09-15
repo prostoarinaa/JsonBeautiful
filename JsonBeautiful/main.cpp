621d98ab53d76123d930b52dde0f37945e544763
Motorina Arina <arinamotorinaaaaa@gmail.com>
2021-09-16 00:14:01 +0300

    Update main.cpp

--- JsonBeautiful/main.cpp
+++ JsonBeautiful/main.cpp
@@ -6,9 +6,96 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code hhhhere...
-    std::cout << "Hello, World!\n";
+#include <fstream>
+#include <string>
+//class Beautiful {
+//
+//};
+using namespace std;
+int main()
+{
+    ifstream fileOld;
+    ofstream fileNew;
+    fileOld.open("/Users/pk/Desktop/OI/TASKS/JsonBeautiful/jsonstring.txt");
+    fileNew.open("/Users/pk/Desktop/OI/TASKS/JsonBeautiful/JsonBeautiful/jsonstringNew.txt");
+    cout << "hjkhkj" << endl;
+    if (!fileOld.is_open()) {
+        cout << "no" << endl;
+    }
+    else {
+        cout << "yes"
+        <<endl;
+    }
+    if (!fileNew.is_open()) {
+        cout << "no" << endl;
+    }
+    else {
+        cout << "yes"
+        <<endl;
+    }
+    string str = "";
+    while(true)
+    {
+        getline(fileOld, str, '[');
+        fileNew << str;
+        if (!fileOld.eof())
+            fileNew << "[\n";
+        else break;
+    }
+//    ifstream input("/Users/pk/Desktop/OI/TASKS/JsonBeautiful/JsonBeautiful/jsonstringNew.txt"); //Открываешь первый файл для чтения
+//        ofstream output("/Users/pk/Desktop/OI/TASKS/JsonBeautiful/jsonstring.txt"); //Открываешь второй файл для записи
+//        output << input.rdbuf();
+//    
+//    while(true)
+//    {
+//        getline(input, str, ']');
+//        fileNew << str;
+//        if (!input.eof())
+//            output << "]\n";
+//        else break;
+//    }
+//    while(true)
+//    {
+//        getline(fileOld, str, '{');
+//        fileNew << str;
+//        if (!fileOld.eof())
+//            fileNew << "{\n";
+//        else break;
+//    }
+//    while(true)
+//    {
+//        getline(fileOld, str, '}');
+//        fileNew << str;
+//        if (!fileOld.eof())
+//            fileNew << "}\n";
+//        else break;
+//    }
+//    while(true)
+//    {
+//        getline(fileOld, str, ':');
+//        fileNew << str;
+//        if (!fileOld.eof())
+//            fileNew << ":  ";
+//        else break;
+//    }
+    fileOld.close();
+    fileNew.close();
+//    input.close();
+//    output.close();
+//    while (getline(fileOld, str))
+//    {
+//        s += str;
+//    }
+//    int i = -1;
+//    while (i < str.size())
+//    {
+//        i++;
+//        switch(str[i])
+//        {
+//            case '[':
+//                str[]
+//        }
+//
+//    }
     return 0;
 }
